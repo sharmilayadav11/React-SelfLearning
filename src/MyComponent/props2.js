73ca463266b68151db91a50910237ef7213b1f10
Sharmila Yadav <122268263+sharmilayadav11@users.noreply.github.com>
2024-02-29 09:22:04 +0200

    Create props2.js

---
+++ src/MyComponent/props2.js
@@ -0,0 +1,15 @@
+function Car(props) {
+    return <h2>I am a {props.brand}!</h2>;
+}
+
+function Garage() {
+    return (
+        <>
+            <h1>Who lives in my garage?</h1>
+            <Car brand="Ford" />
+        </>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Garage />);
