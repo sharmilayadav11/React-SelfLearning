2116bc70dbcd46a3dae5cebeca7bf5a0ffd54119
Sharmila Yadav <122268263+sharmilayadav11@users.noreply.github.com>
2024-08-26 13:39:50 +0300

    Create Theory.js
    What is React?

---
+++ src/MyComponent/Theory.js
@@ -0,0 +1,4 @@
+How does React Work ?
+    React creates a VIRTUAL DOM in memory.
+
+Instead of manipulating the browser's DOM directly, React creates a virtual DOM in memory, where it does all the necessary manipulating, before making the changes in the browser DOM.
