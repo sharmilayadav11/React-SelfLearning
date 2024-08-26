c2a87bd762435c282440a71ea2d91e1426b873ad
Sharmila Yadav <122268263+sharmilayadav11@users.noreply.github.com>
2024-08-26 13:41:36 +0300

    Update Theory.js

--- src/MyComponent/Theory.js
+++ src/MyComponent/Theory.js
@@ -1,4 +1,5 @@
 How does React Work ?
+    React, sometimes referred to as a frontend JavaScript framework
     React creates a VIRTUAL DOM in memory.
 
 Instead of manipulating the browser's DOM directly, React creates a virtual DOM in memory, where it does all the necessary manipulating, before making the changes in the browser DOM.
