0dbeee0c24676654b6a380f5b2bc42ad132fa2a0
Sharmila Yadav <122268263+sharmilayadav11@users.noreply.github.com>
2024-08-27 12:21:02 +0300

    Update Theory.js

--- src/MyComponent/Theory.js
+++ src/MyComponent/Theory.js
@@ -3,3 +3,9 @@ How does React Work ?
     React creates a VIRTUAL DOM in memory.
 
 Instead of manipulating the browser's DOM directly, React creates a virtual DOM in memory, where it does all the necessary manipulating, before making the changes in the browser DOM.
+To use React in production, you need npm which is included with Node.js.
+
+To get an overview of what React is, you can write React code directly in HTML.
+
+But in order to use React in production, you need npm and Node.js installed.
+The quickest way start learning React is to write React directly in your HTML files.
